33b5504fc1d28a9cf3379b81e985e04f9bd14f51
Clement Andrew <143867839+Clem0-o7@users.noreply.github.com>
2025-03-11 11:25:01 +0530

    Payment PDF Updated

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -259,7 +259,7 @@ const Profile = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-2">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                 </svg>
-                <Button sx={{ marginLeft: "10px" }} onClick={() => window.open("https://techutsav2024.blob.core.windows.net/eventdetails/Techutsav2024Paymentprocess.pdf")}>
+                <Button sx={{ marginLeft: "10px" }} onClick={() => window.open("https://clement2004.blob.core.windows.net/techutsav25/Payment_process_techutsav2025.pdf")}>
                   <CloudDownloadIcon />
                 </Button>
               </div>
